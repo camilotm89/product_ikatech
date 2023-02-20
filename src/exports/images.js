5239459fab02959b2afbe71a8c8cf6ad1d93ae46
camilotm89 <camilotm89@gmail.com>
2023-02-19 23:33:25 -0500

    upload photos

--- src/exports/images.js
+++ src/exports/images.js
@@ -8,7 +8,17 @@ import image7 from '../assets/img/7.jpg'
 import image8 from '../assets/img/8.jpg'
 import image9 from '../assets/img/9.jpg'
 import image10 from '../assets/img/10.jpg'
+import image11 from '../assets/img/11.jpg'
+import image12 from '../assets/img/12.jpg'
+import image13 from '../assets/img/13.jpg'
+import image14 from '../assets/img/14.jpg'
+import image15 from '../assets/img/15.jpg'
+import image16 from '../assets/img/16.jpg'
+import image17 from '../assets/img/17.jpg'
+import image18 from '../assets/img/18.jpg'
+import image19 from '../assets/img/19.jpg'
 
-const imageArray = [image1,image2, image3, image4, image5, image6, image7, image8, image9, image10];
+
+const imageArray = [image1,image2, image3, image4, image5, image6, image7, image8, image9, image10, image11, image12,image13,image14,image15,image16,image17,image18,image19];
 
 export default imageArray;
